afad9dbbab50c257e15e7486249dbb7896951285
Chick Chicky <91527355+ChickChicky@users.noreply.github.com>
2022-11-02 11:41:00 +0100

    added turtle installer

---
+++ install.lua
@@ -0,0 +1,5 @@
+shell.run('wget https://raw.githubusercontent.com/ChickChicky/cc-turtle-remote/main/turtle/startup.lua')
+shell.run('wget https://raw.githubusercontent.com/ChickChicky/cc-turtle-remote/main/turtle/json.lua')
+shell.run('wget https://raw.githubusercontent.com/ChickChicky/cc-turtle-remote/main/turtle/turtle_wrapper.lua')
+shell.run('wget https://raw.githubusercontent.com/ChickChicky/cc-turtle-remote/main/turtle/websocket.lua')
+shell.run('wget https://raw.githubusercontent.com/ChickChicky/cc-turtle-remote/main/turtle/data.json')
